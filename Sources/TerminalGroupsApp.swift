d270d73f983c6899f9a8fbd6c1dc1b902b687452
Doug Hammond <d.lakehammond@gmail.com>
2025-10-19 01:04:04 +0200

    Fix keyboard focus - force app activation on launch
    - Set activation policy to .regular (makes it a regular GUI app)
    - Call activate(ignoringOtherApps: true) to steal focus
    - This fixes issue where keyboard input went to previous app
    - App now properly shows in menu bar and accepts keyboard input
    
    Fixes: Keyboard input not working, app not stealing focus

--- Sources/TerminalGroupsApp.swift
+++ Sources/TerminalGroupsApp.swift
@@ -24,6 +24,13 @@ struct TerminalGroupsApp: App {
 
 /// App delegate to handle lifecycle events
 class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Force the app to activate and accept keyboard input
+        // This is needed when running as a Swift Package executable
+        NSApplication.shared.setActivationPolicy(.regular)
+        NSApplication.shared.activate(ignoringOtherApps: true)
+    }
+
     func applicationWillTerminate(_ notification: Notification) {
         // Save state on quit
         // Note: We need a reference to AppState here
